a88bf010b8ff78cea2929cf72e253f4d1d5ef44c
kushagra-singhal <kushagra.singhalzemosolabs.com>
2022-02-16 22:33:36 +0530

    chore-Added section 4 to corejava branch

---
+++ Core-Java/Section4-Projects/src/Proj4_1_Arrays.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Proj4_1_Arrays {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        int[] someValues = new int[5];
+
+        for (int i = 0; i < someValues.length; i++){
+            System.out.print("enter an Integer\t");
+            someValues[i] = keyboard.nextInt();
+        }// end for
+
+        for (int i = 0; i< someValues.length; i++){
+
+            System.out.println("result is: " + someValues[i] * 2 );
+        }
+    }
+}
