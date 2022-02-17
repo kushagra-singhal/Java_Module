4e7ab1e70a1d6c2aaac8003cb3864b3ee789083d
kushagra-singhal <kushagra.singhalzemosolabs.com>
2022-02-17 23:21:14 +0530

    added assignment1

---
+++ JavaAssignments/src/Assignments/Assignment2/ContainAToZ.java
@@ -0,0 +1,45 @@
+package Assignments.Assignment2;
+
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class ContainAToZ {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the input String");
+
+        String inputString = scanner.nextLine();
+
+        boolean checker = checkString(inputString);
+
+        if(checker)
+        {
+            System.out.println("It contains all a to z characters");
+        }
+        else
+            System.out.println("It does not contain all a to z characters");
+
+    }
+
+    private static boolean checkString(String inputString)
+    {
+        HashSet<Character> characters = new HashSet<>();
+        if(inputString.length() >= 25) {
+            for (int i = 0; i < inputString.length(); i++) {
+                char tempChar = inputString.charAt(i);
+
+                if ((tempChar >= 'a' && tempChar <= 'z' || tempChar >= 'A' && tempChar <= 'Z') && (characters.size() <= 25)) {
+                    characters.add(Character.toUpperCase(tempChar));
+                }
+            }
+
+            return characters.size() == 26;
+        }
+        else
+            return false;
+    }
+
+}
