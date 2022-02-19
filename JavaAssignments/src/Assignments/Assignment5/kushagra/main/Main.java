ed695d9e58738cc9dfdf2792cc652d0647b00666
kushagra-singhal <kushagra.singhalzemosolabs.com>
2022-02-19 23:36:55 +0530

    added upto assignment5

---
+++ JavaAssignments/src/Assignments/Assignment5/kushagra/main/Main.java
@@ -0,0 +1,23 @@
+package Assignments.Assignment5.kushagra.main;
+
+import Assignments.Assignment5.kushagra.data.DefaultInitializationassignment;
+import Assignments.Assignment5.kushagra.singleton.DefaultStatic;
+
+public class Main {
+
+
+    DefaultInitializationAssignment obj1 = new DefaultInitializationAssignment();
+/* A static method belongs to a class rather than an instance thus to access methods of a class it is necessary
+    to create a static method
+ */
+/*
+obj1.printVar();
+obj1.printLocalVar();
+*/
+
+    public static void main(String[] args) {
+
+        DefaultStatic obj2 = new DefaultStatic();
+        obj2.printString();
+    }
+}
